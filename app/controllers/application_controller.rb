565f738021edb859ff6049049becf3e0e77ba550
Htet Naing <htetnaing0814@gmail.com>
2022-11-14 19:45:44 +0630

    removed sign_out comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
-
-  # # Overwriting the sign_out redirect path method
-  # def after_sign_out_path_for(resource_or_scope)
-  #   splash_path
-  # end
-
   protected
 
   def configure_permitted_parameters
