222bfa6592de8046decfb4371a06e0098de0a074
Htet Naing <htetnaing0814@gmail.com>
2022-11-16 15:15:04 +0630

    added user and join table tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :purchases
   has_many :categories
+
+  validates :name,                 presence: true, length: { minimum: 3, maximum: 25 }
+  validates :email, :password,      presence: true
 end
